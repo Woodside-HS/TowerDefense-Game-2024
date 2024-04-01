3bf0edf746e7b6cc7f56ecdeac91b12e2a2662c8
Logan Thomas <103534716+812047@users.noreply.github.com>
2024-04-01 13:38:15 -0700

    b

--- TowerGameJS-Phase4/enemy.js
+++ TowerGameJS-Phase4/enemy.js
@@ -25,7 +25,7 @@ class Enemy {
     this.lastTime = Date.now();
     this.coolDown = 1000;
     this.towerLoc = vector2d(0, 0);
-    this.health = 1000;
+    this.health;
     this.targetCell = this.nextTarget();
     this.target = this.targetCell.center;
     this.shape = "circle";
@@ -55,8 +55,7 @@ class Enemy {
     this.liquifyUpgradedImmunities = [false, "targetable"];
     this.missileImmunities = [false, "targetable"];
     this.missileUpgradedImmunities = [false, "targetable"];
-    this.buffregenImmunities = [false, "targetable"];
-    this.buffregenUpgradedImmunities = [false, "targetable"];
+
   }
 
   run() {
@@ -140,7 +139,7 @@ class Enemy {
             this.health -= 800 * towerGame.missiles[h].damageMult;//this does not current work
             towerGame.missiles.splice(h, 1);
           }
-        } else if (!this.missileImmunities[0]) {
+        } else if (!this.missileUpgradedImmunities[0]) {
           this.health -= 800 * towerGame.missiles[h].damageMult;//this does not current work
           towerGame.missiles.splice(h, 1);
         }
@@ -230,7 +229,7 @@ class Enemy {
           }
         }
         else if (towerGame.bullets[h].ability == "explosive") {
-          if (!this.explosiveImmunities[0]) {
+          if (!this.explosiveImmunities[0] || !this.explosiveUpgradedImmunities[0]) {
             this.health -= 100 * towerGame.bullets[h].damageMult;
             if (this.health <= 0) {
               this.kill = true;
@@ -269,56 +268,56 @@ class Enemy {
 
     }
 
-    if (this.loc.dist(this.target) <= this.vel) {    // if we have reached the current target
+    if (this.loc.dist(this.target) <= this.vel.mag()) { // if we have reached the current target
       this.currentCell = this.targetCell;
-      if (this.currentCell == this.game.root) {   // we have reached the end of the path
-        this.kill = true;
-        towerGame.health = towerGame.health - 1;
-        return;
+      if (this.currentCell == this.game.root) { // we have reached the end of the path
+          this.kill = true;
+          towerGame.health = towerGame.health - 1;
+          return;
       }
       if (!this.hitByFreezeUpgraded) {
-        this.targetCell = this.nextTarget();// set a new target
+          this.targetCell = this.nextTarget(); // set a new target
       } else {
-        let random = Math.floor(Math.random() * 5);
-        if (random < 3) {
-          this.targetCell = this.oppositeNextTarget();
-        } else {
-          this.targetCell = this.nextTarget();
-        }
+          let random = Math.floor(Math.random() * 5);
+          if (random < 3) {
+              this.targetCell = this.oppositeNextTarget();
+          } else {
+              this.targetCell = this.nextTarget();
+          }
       }
       if (!this.targetCell) {
-        this.kill = true;   // can happen if user blocks cells while enemies are attacking
-        return;
+          this.kill = true; // can happen if user blocks cells while enemies are attacking
+          return;
       }
-      this.target = this.targetCell.center;      // always target the center of the cell
-    }
-    // calculate new vector from current location to the target.
-    var targetVec = this.target.copy().sub(this.loc);    // the direction we want to go
-    var angleBetween = this.velVec.angleBetween(targetVec);
-    if (angleBetween) {  // if there is some angle between
-      if (angleBetween > 0 && angleBetween > Math.PI)  // positive and > 180 degrees
-        angleBetween = angleBetween - 2 * Math.PI;   // make negative and < 180 degrees
-      else if (angleBetween < 0 && angleBetween < -Math.PI)   // negative and < -180 degrees
-        angleBetween = angleBetween = angleBetween + 2 * Math.PI;  // make positive and < 180 degrees
-
-      // now rotate the current velocity in the direction of the targetAngle
-      // a little at a time
-      this.velVec.rotate(angleBetween / 2);
-      this.angle = this.velVec.angle();
-    }
-    if (this.slowed < 1) {//the third guy does this
-      this.count++;
-      if (this.count == 3) {
-        this.loc.add(this.velVec)//this make it look extremely jittery 
-        //I will eventually do a complete overhaul of the velocity to get with better.
-        //(proboly after all the other towers are decent)
-        this.count = 0;
+      this.target = this.targetCell.center; // always target the center of the cell
+  } else {
+      // calculate new vector from current location to the target.
+      var targetVec = this.target.copy().sub(this.loc); // the direction we want to go
+      var angleBetween = this.vel.angleBetween(targetVec);
+      if (angleBetween) { // if there is some angle between
+          if (angleBetween > 0 && angleBetween > Math.PI) // positive and > 180 degrees
+              angleBetween = angleBetween - 2 * Math.PI; // make negative and < 180 degrees
+          else if (angleBetween < 0 && angleBetween < -Math.PI) // negative and < -180 degrees
+              angleBetween = angleBetween = angleBetween + 2 * Math.PI; // make positive and < 180 degrees
+  
+          // now rotate the current velocity in the direction of the targetAngle
+          // a little at a time
+          this.vel.rotate(angleBetween / 2);
+          this.angle = this.vel.angle();
       }
-    } else if (this.slowed > 1) {
-      this.loc.add(this.velVec);
-    }          // apply velocity to location
+      if (this.slowed < 1) { //the third guy does this
+          this.count++;
+          if (this.count == 3) {
+              this.loc.add(this.vel); //this make it look extremely jittery
+              //I will eventually do a complete overhaul of the velocity to get with better.
+              //(proboly after all the other towers are decent)
+              this.count = 0;
+          }
+      } else if (this.slowed > 1) {
+          this.loc.add(this.vel);
+      } // apply velocity to location
   }
-
+  }
   checkCollide(shape1, shape2) {
 
     if (shape1.shape === "circle") {
@@ -400,37 +399,72 @@ class Enemy {
 //false if it can't hit the enemy so false of default means yes you can hit
 //targetable is basically if the enemy is immune to the towers attack the tower will still target it unless the targetable
 // is set to something that is not targetable
+
+
+//circle radius slows down enemies
+//death explotion of high level enemies has a chance to completely destroy a nearby tower
+//basic enemy normal fish
+//plankton (lots of tiny ones that die gradually from damage)
+//turtle immunities 
+//fast shark
+//
+
+
+// this.normalImmunities = [false, "targetable"];
+// this.normalUpgradedImmunities = [false, "targetable"];
+// this.fastImmunities = [false, "targetable"];
+// this.fastUpgradedImmunities = [false, "targetable"];
+// this.freezeImmunities = [false, "targetable"];
+// this.freezeUpgradedImmunities = [false, "targetable"];
+// this.explosiveImmunities = [false, "targetable"];
+// this.explosiveUpgradedImmunities = [false, "targetable"];
+// this.rayImmunities = [false, "targetable"];
+// this.rayUpgradedImmunities = [false, "targetable"];
+// this.cannonImmunities = [false, "targetable"];
+// this.cannonUpgradedImmunities = [false, "targetable"];
+// this.bladeStormImmunities = [false, "targetable"];
+// this.bladeStormUpgradedImmunities = [false, "targetable"];
+// this.liquifyImmunities = [false, "targetable"];
+// this.liquifyUpgradedImmunities = [false, "targetable"];
+// this.missileImmunities = [false, "targetable"];
+// this.missileUpgradedImmunities = [false, "targetable"];
 class Enemy1 extends Enemy {
   constructor(game) {
     super(game);
     this.img = Enemy.image1;
+    this.health = 1000;
+    this.normalSmallEnemy = true;
   }
 }
 class Enemy2 extends Enemy {
   constructor(game) {
     super(game);
     this.img = Enemy.image2;
+    this.health = 2000;
+    this.normalEnemy = true;
   }
 }
 class Enemy3 extends Enemy {
   constructor(game) {
     super(game);
     this.img = Enemy.image3;
-    this.health = 5000;
+    this.health = 4000;
+    this.freezeEnemy = true;
   }
 }
 class Enemy4 extends Enemy {
   constructor(game) {
     super(game);
     this.img = Enemy.image4;
-    this.health = 15000;
+    this.health = 4000;
+    this.explosiveEnemy = true;
   }
 }
 class Enemy5 extends Enemy {
   constructor(game) {
     super(game)
     this.img = Enemy.image5;
-    this.randomPath = 0;
-    this.health = 1000000000000000000;
+    this.health = 10000;
+    this.turtleEnemy = true;
   }
 }
