97125ece05dbc9646db9a0c73b96051cc5902ce5
Logan Thomas <103534716+812047@users.noreply.github.com>
2024-04-15 09:25:58 -0700

    g

--- TowerGameJS-Phase4/blade.js
+++ TowerGameJS-Phase4/blade.js
@@ -1,16 +1,16 @@
 class Blade {
 
-  constructor(location, bImg, angle, type, blades, damageMult, isOrginal) {
+  constructor(location, bImg, type, blades, damageMult, isOrginal) {
     this.loc = new vector2d(0, 0);
     this.towerLoc = location;
     this.shape = "square";
     this.img = bImg;
     this.ability = type;
-    this.orbitalRadius = 7;
+    this.orbitalRadius = 0;
     this.isOrginal = isOrginal;
     this.angle = 0;
-    this.angularVelocity = 0.1;
-    this.w = this.img.width * 1.5;
+    this.angularVelocity = 0.05;
+    this.w = this.img.width * 1.0;
     this.blades = blades;
     this.damageMult = damageMult;
   }
@@ -25,27 +25,27 @@ class Blade {
     if (this.isOrginal == "first") {
       var ctx = towerGame.context;
       ctx.save();
-      ctx.translate(this.towerLoc.x, this.towerLoc.y);
-      ctx.rotate((Math.PI * 0.8) + Math.PI * this.blades * 0.53);//idk the png is scuffed so its not perfect
+      ctx.translate(this.towerLoc.x + 0, this.towerLoc.y + 0)
+      ctx.rotate(this.blades * 0.5 * Math.PI);//idk the png is scuffed so its not perfect
       // but I can not be asked to fix the png yet
       ctx.rotate(this.angle)
 
 
-      ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2,
-        -this.img.width * 1.5, -this.img.height * 1.5);
+      ctx.drawImage(this.img, 0 , 0,
+        this.img.width/2, this.img.height/2);
 
       ctx.restore();
     } else {
       var ctx = towerGame.context;
       ctx.save();
       ctx.translate(this.towerLoc.x, this.towerLoc.y);
-      ctx.rotate(Math.PI * 0.46 * this.blades * 0.53);//idk the png is scuffed so its not perfect
+      ctx.rotate(this.blades * 0.25 * Math.PI);//idk the png is scuffed so its not perfect
       // but I can not be asked to fix the png yet
       ctx.rotate(this.angle)
 
 
-      ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2,
-        -this.img.width * 1.5, -this.img.height * 1.5);
+      ctx.drawImage(this.img, 0, 0,
+        this.img.width/2, this.img.height/2);
 
       ctx.restore();
     }
--- TowerGameJS-Phase4/resources/images/spritesheets/sprites.js
+++ TowerGameJS-Phase4/resources/images/spritesheets/sprites.js
@@ -227,11 +227,11 @@ json = {"frames": {
 },
 "p70000":
 {
-	"frame": {"x":2,"y":171,"w":27,"h":20},
+	"frame": {"x":0,"y":176,"w":33,"h":248},
 	"rotated": false,
 	"trimmed": false,
-	"spriteSourceSize": {"x":0,"y":0,"w":27,"h":20},
-	"sourceSize": {"w":27,"h":20}
+	"spriteSourceSize": {"x":0,"y":0,"w":33,"h":248},
+	"sourceSize": {"w":33,"h":248}
 },
 "p80000":
 {
