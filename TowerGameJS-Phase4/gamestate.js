7229a89e5c196ba0e7651ab2d05dc9db435e5744
Logan Thomas <103534716+812047@users.noreply.github.com>
2024-05-15 09:55:37 -0700

    Update gamestate.js

--- TowerGameJS-Phase4/gamestate.js
+++ TowerGameJS-Phase4/gamestate.js
@@ -137,7 +137,7 @@ class GameState5 extends GameState { // game itself
     super(game)
     this.game.health = 100;
     this.game.score = 0
-    this.game.bankValue = 400;
+    this.game.bankValue = 0;
     this.game.gameTime = 0;
     this.game.grid = [];
     this.game.towers = [];
