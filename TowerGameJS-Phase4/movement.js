d23da22fe59600afc90eb8890db6f7b5beab33dc
Logan Thomas <103534716+812047@users.noreply.github.com>
2024-05-15 11:11:39 -0700

    n

--- TowerGameJS-Phase4/movement.js
+++ TowerGameJS-Phase4/movement.js
@@ -1,28 +1,26 @@
 class Movement {
     constructor(loc, target, speed) {
-        this.loc = loc; // Initial location
-        this.target = target; // Target location
-        this.speed = speed; // Movement speed
-        this.finished = false; // Flag to indicate if movement is finished
+        this.loc = loc; 
+        this.target = target;
+        this.speed = speed; 
+        this.finished = false; 
     }
 
     update() {
         if (!this.finished) {
-            // Calculate the direction vector from loc to target
+
             let direction = this.target.copy().sub(this.loc);
             direction.normalize();
-            // Calculate the distance between loc and target
+
             let distance = this.loc.dist(this.target);
 
-            // Calculate the displacement for this update based on speed
             let displacement = direction.multiply(this.speed);
-            // If the displacement is greater than or equal to the distance to the target, we have reached the target
+
             if (displacement.length() >= distance) {
-                this.loc = this.target.copy(); // Set loc location to target
-                this.finished = true; // Set finished flag to true
+                this.loc = this.target.copy(); 
+                this.finished = true; 
             } else {
-                // Move loc location by displacement
-               
+            
                  this.loc.add(displacement);
   
             }
@@ -31,8 +29,8 @@ class Movement {
 
     setTarget(loc, newTarget) {
         this.loc = loc;
-        this.target = newTarget; // Update target location
-        this.finished = false; // Reset finished flag
+        this.target = newTarget;
+        this.finished = false; 
     }
 
 }
