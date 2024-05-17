c5432bae98056b7f7199184c551c6b9c12bfbe15
Logan Thomas <103534716+812047@users.noreply.github.com>
2024-05-17 10:06:33 -0700

    g

--- TowerGameJS-Phase4/wave.js
+++ TowerGameJS-Phase4/wave.js
@@ -20,7 +20,7 @@ class Wave {
     let NumberOfEnemiesInWave = 0;
     for(let i = 0; i < towerGame.waves[this.waveNumber].length; i ++){
        if(towerGame.numWave < towerGame.waves[this.waveNumber].length-1){
-      towerGame.numWave += towerGame.waves[this.waveNumber][i];
+      NumberOfEnemiesInWave += towerGame.waves[this.waveNumber][i];
        } else {
          towerGame.gameState = new GameState3(towerGame, "win");
        }
